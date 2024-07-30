e6ee3d78b443d9f7de99bae0e54216ed425c9f8b
Trevor Jewkes <trevorjewkes6@gmail.com>
2024-07-29 20:52:50 -0600

    Added male calculator

--- Exercise/BodyFatCalc/scripts/main.js
+++ Exercise/BodyFatCalc/scripts/main.js
@@ -1,7 +1,7 @@
 function handleRadioButton(radioButton) {
-	alert('Got button: ' + radioButton.value)
 	maleDiv = document.getElementById('maleForm')
 	femaleDiv = document.getElementById('femaleForm')
+	document.getElementById('mBodyFat').style.display = 'none'
 	if (radioButton.value === 'Male') {
 		maleDiv.style.display = 'block'
 		femaleDiv.style.display = 'none'
@@ -10,4 +10,14 @@ function handleRadioButton(radioButton) {
 		maleDiv.style.display = 'none'
 		femaleDiv.style.display = 'block'
 	}
+}
+
+function calculateMaleBodyWeight() {
+	weight = Number(document.getElementById('mBodyWeight').value)
+	girth = Number(document.getElementById('mWaistGirth').value)
+	result1 = (weight * 1.082) + 94.42
+	leanBodyWeight = result1 - (girth * 4.15)
+	finalResult = ((weight - leanBodyWeight) * 100) / weight
+	document.getElementById('mBodyFat').innerText = 'Body Fat Percentage: ' + finalResult + '%'
+	document.getElementById('mBodyFat').style.display = 'block'
 }
