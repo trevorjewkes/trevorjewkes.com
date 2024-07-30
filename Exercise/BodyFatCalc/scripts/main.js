5e03e6ef505d0daf509f567b300e4dcac67c9df0
Trevor Jewkes <trevorjewkes6@gmail.com>
2024-07-29 20:19:33 -0600

    debugging

--- Exercise/BodyFatCalc/scripts/main.js
+++ Exercise/BodyFatCalc/scripts/main.js
@@ -1,6 +1,7 @@
 function handleRadioButton(radioButton) {
 	maleDiv = document.getElementById('maleForm')
 	femaleDiv = document.getElementById('femaleForm')
+	alert(radioButton.value)
 	if (radioButton.value === 'Male') {
 		maleDiv.style.display = 'block'
 		femaleDiv.style.display = 'none'
